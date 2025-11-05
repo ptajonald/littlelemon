e6612554d1d7940ce9a555975355e19c403a5791
fatherjonald <ronjohn1216@gmail.com>
2025-11-05 21:40:49 +0000

    updated content on menu page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,7 +3,28 @@ import Footer from "../components/Footer";
 function AboutPage() {
   return (
     <>
-      <p>about page</p>
+      <section id="about">
+        <div id="about-left">
+          <h3>Little Lemon</h3>
+          <h2>Cleveland</h2>
+          <p>
+            Hours: <br />
+            Sunday-Thursday: 10AM-10PM <br />
+            Friday-Saturday: 10AM-12AM
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue
+            tortor ut mi pellentesque consectetur. Nullam tristique dolor at
+            erat volutpat, feugiat porta augue aliquam. Aliquam nulla elit,
+            tincidunt non urna et, vestibulum facilisis arcu. Nullam dapibus
+            semper justo, id.
+          </p>
+        </div>
+        <div id="about-right">
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+      </section>
       <Footer />
     </>
   );
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,9 +1,76 @@
 import Footer from "../components/Footer";
+import salad from "../images/cat-csalad.jpg";
+import bruchetta from "../images/bruschetta1.jpg";
+import lemon from "../images/lemon.jpg";
+import "../styles/Menu.css";
 
 function Menu() {
   return (
     <>
-      <p>menu</p>
+      <section id="menu">
+        {/* <div id="specials">
+          <h2>Specials</h2>
+          <button type="button" className="btn btn-warning btn-lg">
+            View More
+          </button>
+        </div> */}
+        <div id="title">
+          <h1>Menu</h1>
+        </div>
+        <div id="entrees">
+          <div className="entree">
+            <img src={salad} alt="salad" className="" />
+            <section>
+              <div className="name-price">
+                <h4>Greek Salad</h4>
+                <h4>$12.99</h4>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                cursus, ligula sed rutrum volutpat, lorem nisl egestas dui, ut
+                mollis dolor sem id odio. Cras.
+              </p>
+              <button type="button" class="btn btn-success">
+                Order Delivery
+              </button>
+            </section>
+          </div>
+          <div className="entree">
+            <img src={bruchetta} alt="bruchetta" />
+            <section>
+              <div className="name-price">
+                <h4>Bruchetta</h4>
+                <h4>$5.99</h4>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                cursus, ligula sed rutrum volutpat, lorem nisl egestas dui, ut
+                mollis dolor sem id odio. Cras.
+              </p>
+              <button type="button" class="btn btn-success">
+                Order Delivery
+              </button>
+            </section>
+          </div>
+          <div className="entree">
+            <img src={lemon} alt="lemon" />
+            <section>
+              <div className="name-price">
+                <h4>Lemon Cake</h4>
+                <h4>$4.99</h4>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                cursus, ligula sed rutrum volutpat, lorem nisl egestas dui, ut
+                mollis dolor sem id odio. Cras.
+              </p>
+              <button type="button" class="btn btn-success">
+                Order Delivery
+              </button>
+            </section>
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
